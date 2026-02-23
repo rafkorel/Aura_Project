7ea162ccc2a1eaaf785587d0984991b171446f38
Ilya <Boldyrevv.ilya2007@gmail.com>
2026-02-23 19:45:10 +0700

    Добавлен сервер на FastAPI с эндпоинтами / и /health

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Первая попытка повторного освоения GIT")
+from fastapi import FastAPI
+
+# Создаём приложение
+app = FastAPI()
+
+# Этот эндпоинт отвечает на главной странице
+@app.get("/")
+def root():
+    return {"message": "Привет, мир! Это Aura Project"}
+
+# Этот эндпоинт проверяет, жив ли сервер
+@app.get("/health")
+def health():
+    return {"status": "healthy", "version": "0.0.1"}
